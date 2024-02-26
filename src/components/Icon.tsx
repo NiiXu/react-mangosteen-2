f4579e1148577834a79ee11cabd702917817722f
NiiXu <chenhengxv@outlook.com>
2024-02-26 17:36:24 +0800

    完成topnav组件

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,15 +1,11 @@
-import React from 'react'
-import s from './Icon.module.scss'
 import c from 'classnames'
-
 interface Props {
     className?: string
     name: string
 }
-
-export const Icon: React.FC<Props> = ({name, className}) => {
+export const Icon: React.FC<Props> = ({ name, className }) => {
     return (
-        <svg className={c(className, 'icon')}>
+        <svg className={c(className, 'j-icon')}>
             <use xlinkHref={`#${name}`}></use>
         </svg>
     )
