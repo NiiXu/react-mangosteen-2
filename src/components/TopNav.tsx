1674780d8dd9a6f1f6f8287c8c1d00c3a3b00020
NiiXu <chenhengxv@outlook.com>
2024-02-29 15:31:05 +0800

    完成滑动菜单的样式

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,17 +1,17 @@
 import React, {useContext} from 'react';
 import {Icon} from './Icon';
-import {menuContext} from '../contexts/menuContext';
+import {useMenuStore} from '../stores/useMenuStore';
 
 interface Props {
     title?: string
 }
 
-export const TopNav: React.FC<Props> = ({title = '山竹记账'}) => {
-    const { setVisible } = useContext(menuContext)
+export const TopNav: React.FC<Props> = ({ title = '山竹记账'}) => {
+    const { visible, setVisible } = useMenuStore()
     return (
         <div text-white flex items-center pt-24px pb-8px px-24px>
             <Icon name="menu" className="w-24px h-24px mr-16px"
-            onClick={ ()=> { setVisible(true) }} />
+            onClick={ ()=> { setVisible(!visible) }} />
             <h1 text-24px>{title}</h1>
         </div>
     )
--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,17 +1,18 @@
 import React, {useState} from 'react'
-import { AddItemFloatButton } from '../components/AddItemFloatButton'
-import { TopNav } from '../components/TopNav'
+import {AddItemFloatButton} from '../components/AddItemFloatButton'
+import {TopNav} from '../components/TopNav'
+import type {TimeRange} from '../components/TimeRangePicker'
 import {TimeRangePicker} from '../components/TimeRangePicker'
 import {ItemsSummary} from './ItemsPage/ItemsSummary'
 import {ItemsList} from './ItemsPage/ItemsList'
-import type { TimeRange } from '../components/TimeRangePicker'
 import styled from 'styled-components'
-import {TopMenu} from '../components/TopMenu';
-import { menuContext } from '../contexts/menuContext'
+import {TopMenu} from '../components/TopMenu'
+import {useMenuStore} from '../stores/useMenuStore'
+import { Icon } from '../components/Icon'
 
 const Div = styled.div`
-  background: linear-gradient(0deg, rgba(143,76,215,1) 0%, rgba(92,51,190,1) 100%);
-  `
+  background: linear-gradient(0deg, rgba(143, 76, 215, 1) 0%, rgba(92, 51, 190, 1) 100%);
+`
 
 export const ItemsPage: React.FC = () => {
     const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
@@ -19,7 +20,7 @@ export const ItemsPage: React.FC = () => {
         {
             id: 1,
             user_id: 1,
-            kind:'incomes',
+            kind: 'incomes',
             amount: 1000,
             tag_ids: [1],
             happen_at: '2023-01-01T00:00:00.000Z',
@@ -29,7 +30,7 @@ export const ItemsPage: React.FC = () => {
         {
             id: 2,
             user_id: 2,
-            kind:'incomes',
+            kind: 'incomes',
             amount: 1000,
             tag_ids: [1],
             happen_at: '2023-01-01T00:00:00.000Z',
@@ -39,19 +40,17 @@ export const ItemsPage: React.FC = () => {
     ])
     //上下文===局部的全局变量
     //全局变量===全局的上下文
-    const [visible,setVisible] = useState(false)
+    const { visible } = useMenuStore()
     return (
         <div>
-            <menuContext.Provider value={{ setVisible }}>
-                <Div>
-                    <TopNav/>
-                    <TimeRangePicker selected={timeRange} onSelected={ setTimeRange }/>
-                </Div>
-                <ItemsSummary/>
-                <ItemsList items={items}/>
-                <AddItemFloatButton/>
-                {visible ? <TopMenu /> : null}
-            </menuContext.Provider>
+            <Div>
+                <TopNav/>
+                <TimeRangePicker selected={timeRange} onSelected={setTimeRange}/>
+            </Div>
+            <ItemsSummary/>
+            <ItemsList items={items}/>
+            <AddItemFloatButton/>
+            {visible ? <TopMenu/> : null}
 
         </div>
     )
