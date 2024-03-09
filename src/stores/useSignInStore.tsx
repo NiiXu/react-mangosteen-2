807b535dc91525c5d8f36e21d8ab4c72f8595cd9
NiiXu <chenhengxv@outlook.com>
2024-03-09 16:25:05 +0800

    添加表单校验

--- src/stores/useSignInStore.tsx
+++ src/stores/useSignInStore.tsx
@@ -1,27 +1,44 @@
 import create from 'zustand'
+import {FormError} from '../lib/validate';
 
-interface Data {
+type Data = {
     email: string
     code: string
 }
-
 interface SignIn {
     data: Data
+    error: FormError<Data>
+    setData: (data: Partial<Data>) => void
+    setError: (error: Partial<FormError<Data>>) => void
 }
 
 export const useSignInStore = create<SignIn>((set, get) => (
     {
         data: {
             email: '',
-            code: ''
+            code: '',
+        },
+        error: {
+            email: [],
+            code: []
         },
         setData: (data: Partial<Data>) => {
             set(state => ({
+                ...state,
                 data: {
                     ...state.data,
                     ...data
                 }
             }))
         },
+        setError: (error: Partial<FormError<Data>>) => {
+            set(state => ({
+                ...state,
+                error: {
+                    ...state.error,
+                    ...error
+                }
+            }))
+        }
     }
 ))
