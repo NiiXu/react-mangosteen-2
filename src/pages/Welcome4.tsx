997058c6f532625c30a0ed2bbe4a3ca65f3b224f
NiiXu <chenhengxv@outlook.com>
2024-03-11 15:51:15 +0800

    点击跳过或者开启应用时，记录用户已经读过广告

--- src/pages/Welcome4.tsx
+++ src/pages/Welcome4.tsx
@@ -1,17 +1,24 @@
+import { useNavigate } from 'react-router-dom'
 import p from '../assets/images/welcome4.svg'
-import {Link} from 'react-router-dom';
+import { useLocalStore } from '../stores/useLocalStore'
 
 export const Welcome4: React.FC = () => {
-  return (
-    <div text-center>
-      <img src={p} w-129px h-83px />
-      <h2 text-32px mt-48px >
-        云备份 <br />
-        再也不怕数据丢失
-      </h2>
-        <div mt-64px>
-            <Link text-32px color="#6035BF" font-bold to="/home">开启应用</Link>
+    const { setHasReadWelcomes } = useLocalStore()
+    const nav = useNavigate()
+    const onSkip = () => {
+        setHasReadWelcomes(true)
+        nav('/welcome/xxx')
+    }
+    return (
+        <div text-center>
+            <img src={p} w-129px h-83px />
+            <h2 text-32px mt-48px >
+                云备份 <br />
+                再也不怕数据丢失
+            </h2>
+            <div mt-64px>
+                <span text-32px color="#6035BF" font-bold onClick={onSkip}>开启应用</span>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
