5bf3305ea1310dfdfd18a7f44e6023bb3361d826
NiiXu <chenhengxv@outlook.com>
2024-03-20 14:32:38 +0800

    创建 新建标签页面

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -7,8 +7,9 @@ import { Welcome2 } from '../pages/Welcome2'
 import { Welcome3 } from '../pages/Welcome3'
 import { Welcome4 } from '../pages/Welcome4'
 import { ItemsPage } from '../pages/ItemsPage'
-import {SignInPage} from '../pages/SignInPage';
-import {ItemsNewPage} from '../pages/ItemsNewPage';
+import { SignInPage } from '../pages/SignInPage';
+import { ItemsNewPage } from '../pages/ItemsNewPage';
+import { TagsNewPage } from '../pages/TagsNewpage'
 export const router = createBrowserRouter([
   { path: '/', element: <Root />, },
   { path: '/home', element: <Home title="首页" /> },
@@ -29,4 +30,5 @@ export const router = createBrowserRouter([
   { path: '/export', element: <div>敬请期待</div> },
   { path: '/tags', element: <div>标签</div> },
   { path: '/noty', element: <div>敬请期待</div> },
+  { path: '/tags/new', element: <TagsNewPage /> },
 ])
